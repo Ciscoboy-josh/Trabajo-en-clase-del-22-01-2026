ad99884b2156ddfacab9f8bfc9116b9c622a7bde
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-01-15 15:48:57 -0600

    Termine el codigo

--- Trabajo.java
+++ Trabajo.java
@@ -4,12 +4,14 @@ public class Trabajo{
         Scanner Scanner = new Scanner(System.in);
         float PI = 3.1416f;
         float AREA;
-
+        
         System.out.println("ingrese el radio de su circulo");
         float radio = Scanner.nextFloat();
 
         AREA=PI*radio*radio;
         System.out.println("El area de su circulo es " + AREA);
+
+        
 
 
     
