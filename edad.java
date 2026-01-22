36a06f73fed2bd3279ad7e514b291a5f19965603
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-01-22 15:34:58 -0600

    agrege las variables y pedi los datos al usuario

---
+++ edad.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class edad{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+
+        System.out.println("Ingresa tu edad");
+        int edad = sc.nextInt();
+        System.out.println("ingresa tu sexo (H = Hombre) y (M =Mujer)");
+        char sexo = sc.next().charAt(0);
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
